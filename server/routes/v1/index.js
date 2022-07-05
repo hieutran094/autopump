3fbc488f403e7a58a135d3e5b13b8d2dce360f9b
hieutran094 <hieutran98vn@gmail.com>
2022-07-05 11:10:27 +0700

    remove express static file

--- server/routes/v1/index.js
+++ server/routes/v1/index.js
@@ -4,9 +4,6 @@ import healthRouter from './health.router.js'
 
 export default (app) => {
   const routers = express.Router()
-  routers.get('/', async (req, res) => {
-    res.render('index.html')
-  })
   routers.use(healthRouter)
   routers.use(settingRouter)
   app.use('/api/v1/', routers)
